148ba3e0f69ff6f65fd9b9724c9a1492748b09d7
Ricardo Zambon <ricardo.zambon@smartm.com>
2020-07-17 23:00:34 -0300

    1st part, 2nd class, video 09.

--- AluraPic/src/app/app.component.ts
+++ AluraPic/src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Alura Pic';
+
+  photos = [
+    {
+      url: 'https://ichef.bbci.co.uk/news/1024/cpsprodpb/E0F0/production/_112548575_gettyimages-492611032-1.jpg',
+      description: 'Lion'
+    },
+    {
+        url: 'https://i.pinimg.com/originals/10/67/e2/1067e2a775d2ccdd2bd2ff8b23c6d969.jpg',
+        description: 'Lioness'
+    }
+  ];
 }
