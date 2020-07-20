e2e8924cd2cd822467b64685aaf383c46860e8b4
Ricardo Zambon <ricardo.zambon@smartm.com>
2020-07-20 19:58:37 -0300

    feat: add route parameter username
    Add router parameter for custom username in PhotoList

--- AluraPic/src/app/photos/photo-list/photo-list.component.ts
+++ AluraPic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { PhotoService } from '../photo/photo.service';
 import { Photo } from '../photo/photo';
 
@@ -11,11 +12,15 @@ export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
 
-  constructor(private photoService: PhotoService) { }
+  constructor(
+    private photoService: PhotoService,
+    private activatedRoute: ActivatedRoute ) { }
 
   ngOnInit(): void {
+    const username = this.activatedRoute.snapshot.params.username;
+
     this.photoService
-      .listFromUser('flavio')
+      .listFromUser(username)
       .subscribe(photos => this.photos = photos);
   }
 
