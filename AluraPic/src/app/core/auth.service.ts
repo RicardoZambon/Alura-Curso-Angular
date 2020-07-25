209d1270616d48099f0df0b53da15b45b31bd143
Ricardo Zambon <ricardo.zambon@smartm.com>
2020-07-25 20:40:43 -0300

    feat: add login and form submit

---
+++ AluraPic/src/app/core/auth.service.ts
@@ -0,0 +1,16 @@
+import { environment } from './../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  authenticate(username: string, password: string): Observable<any> {
+      return this.http.post(`${environment.apiURL}user/login`, { userName: username, password})
+  }
+}
--- AluraPic/src/app/home/signin/signin.component.ts
+++ AluraPic/src/app/home/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from './../../core/auth.service';
 
 @Component({
   templateUrl: './signin.component.html',
@@ -9,13 +10,29 @@ export class SignInComponent implements OnInit {
 
   loginForm: FormGroup;
   
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) { }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       username: ['', Validators.required],
       password: ['', Validators.required]
     });
+  }
+
+  login() {
+    const username = this.loginForm.get('username').value;
+    const password = this.loginForm.get('password').value;
+
+    this.authService
+      .authenticate(username, password)
+      .subscribe(
+        () => console.log('authenticated'),
+        err => {
+          console.log(err);
+          this.loginForm.reset();
+          alert('Invalid username or password');
+        }
+      );
   }
 
 }
