42943a5079a70f683a985d761b00458bafd039a9
Ricardo Zambon <ricardo.zambon@smartm.com>
2020-07-25 20:23:36 -0300

    feat: moved validations to a new component

--- AluraPic/src/app/home/home.module.ts
+++ AluraPic/src/app/home/home.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignInComponent } from './signin/signin.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { VMessageModule } from './../shared/components/vmessage/vmessage.module';
 
 @NgModule({
   declarations: [ SignInComponent ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    VMessageModule
   ]
 })
 export class HomeModule { }
